9e4095c924e87902547d9d96950c53cde3ef28bf
madhavshr <you@example.com>
2016-05-29 22:20:20 +0545

    Add Posts

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,11 +1,20 @@
 class PostsController < ApplicationController
   def index
+    @posts = Post.all.order("created_at DESC")
   end
 
   def new
+    @post = Post.new
+  end
+  
+  def create
+      @post = Post.new(post_params)
+      @post.save
+      redirect_to @post
   end
 
   def show
+    @post = Post.find(params[:id])
   end
 
   def edit
@@ -13,4 +22,11 @@ class PostsController < ApplicationController
 
   def destroy
   end
+
+  private
+  def post_params
+    params.require(:post).permit(:title, :body)
+    
+  end
+    
 end
